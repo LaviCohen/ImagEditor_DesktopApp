6c5df9930e776b219526df90077ccc1a10daab91
lavi cohen <lavicohen78@gamil.com>
2024-07-11 15:14:56 +0300

    Update Version

--- ImagEditor2/src/main/Main.java
+++ ImagEditor2/src/main/Main.java
@@ -52,7 +52,7 @@ public class Main {
 	/**
 	 * Holds the number of the version with minor version (after the decimal point)
 	 */
-	public static final double version = 4.8;
+	public static final double version = 4.9;
 	/**
 	 * The frame of the program.
 	 */
